e4e234233072a01fe982c15d82272f2851c3f722
chenhuangkai <huang.kai@tpisoftware.com>
2022-10-09 21:38:38 +0800

    Authentication - step 2.1 - UsernamePasswordAuthenticationFilter - filter - CustomerAuthenticationFilter extends UsernamePasswordAuthenticationFilter - attemptAuthentication(HttpServletRequest request, HttpServletResponse response)

--- src/main/java/com/example/jwtdemo/config/SecurityConfig.java
+++ src/main/java/com/example/jwtdemo/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.example.jwtdemo.config;
 
+import com.example.jwtdemo.filter.CustomerAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -23,6 +28,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomerAuthenticationFilter(authenticationManager()));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
