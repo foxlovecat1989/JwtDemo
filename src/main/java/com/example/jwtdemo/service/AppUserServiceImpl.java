3d44c87a0f5f8e241a29b053ad41bbefa1264218
chenhuangkai <huang.kai@tpisoftware.com>
2022-10-09 21:18:01 +0800

    Authentication - step 1.2 - UserDetailsService - implements UserDetailsService loadUserByUsername(username)

--- src/main/java/com/example/jwtdemo/service/AppUserServiceImpl.java
+++ src/main/java/com/example/jwtdemo/service/AppUserServiceImpl.java
@@ -4,17 +4,42 @@ import com.example.jwtdemo.model.AppUser;
 import com.example.jwtdemo.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 @Transactional
-public class AppUserServiceImpl implements AppUserService{
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
     private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var appUser = Optional.ofNullable(
+                appUserRepository.findAppUserByUserName(username)).orElseThrow(() -> {
+            var errorMsg = String.format("AppUser Not Found by username: %s", username);
+            log.error(errorMsg);
+            return new UsernameNotFoundException(errorMsg);
+        });
+        log.info("AppUser Found in the database: {}", appUser);
+        Collection<SimpleGrantedAuthority> authorities = appUser.getUserRoles().stream()
+                .map(role -> {
+                    var roleName = role.getRoleName();
+                    return new SimpleGrantedAuthority(roleName);
+                }).toList();
+
+        return new org.springframework.security.core.userdetails.User(
+                appUser.getUserName(), appUser.getPassword(), authorities);
+    }
 
     @Override
     public List<AppUser> saveAllUsers(List<AppUser> appUsers) {
